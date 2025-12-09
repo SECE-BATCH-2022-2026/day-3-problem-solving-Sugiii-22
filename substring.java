0e86650d292ff5cba6edc812242d2444c9f44d5a
Sugi2207 <sugirthakrishnan22@gmail.com>
2025-12-09 09:10:44 +0530

    Add substring.java

---
+++ substring.java
@@ -0,0 +1,40 @@
+import java.io.*;
+
+class Main {
+    public static void main(String args[]) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String inp = br.readLine();
+        Integer n = Integer.parseInt(br.readLine());
+
+        System.out.println(UserMainCode.repeatFront(inp, n));
+        br.close();
+    }
+}
+
+class UserMainCode {
+    public static String repeatFront(String input, Integer n) {
+        int l = input.length();
+
+        if (l == 0) {
+            return input;
+        } 
+        else if (l < 3) {
+            String res = "";
+            for (int i = 0; i < n; i++) {
+                res += input;
+            }
+            return res;
+        } 
+        else {
+            String res = "";
+            String first3 = input.substring(0, 3);
+
+            for (int i = 0; i < n; i++) {
+                res += first3;
+            }
+            return res;
+        }
+    }
+}
